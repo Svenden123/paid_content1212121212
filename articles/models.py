56e9f19ed7e7f1543a8fd067362fe7f665aaf75a
Герман <buhrus2@yandex.ru>
2019-07-16 23:49:00 +0400

    Initial commit

---
+++ articles/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription = models.BooleanField(verbose_name='Подписка')
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+
+    is_paid = models.BooleanField(verbose_name='Платная статья')
+
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.title
+
